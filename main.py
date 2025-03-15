6327208869d4f3ed02e8a0f26abe0d32d5cd8377
Nahom <8nahom8@gmail.com>
2025-03-15 13:01:17 -0500

    reallinged the ui to look better using pady

--- main.py
+++ main.py
@@ -20,19 +20,16 @@ def openFile():
     filename = filedialog.askopenfilename()
     filenameText.configure(text=filename if filename else "No file uploaded")
 
-app.grid_columnconfigure(0, weight=1)
-app.grid_rowconfigure(0, weight=1)
+title = ct.CTkLabel(app, text="Soccer Pass Chart Generator", font=Heavitas, fg_color="transparent")
+title.pack(pady=(30, 10))
 
-title = ct.CTkLabel(app, text="Soccer Pass Chart Generator", width=50, height=50, fg_color="transparent", font=Heavitas)
-title.pack()
+instructionsText = ct.CTkLabel(app, text="Click the button below to open a .csv file of your data", font=HeavitasSmall, fg_color="transparent")
+instructionsText.pack(pady=(10, 10))
 
-instructionsText = ct.CTkLabel(app, text="Click the button below to open a .csv file of your data", fg_color="transparent", font=HeavitasSmall)
-instructionsText.pack()
-
-openFileBtn = ct.CTkButton(app, text="Open file", width=80, height=50, command=openFile)
-openFileBtn.pack()
+openFileBtn = ct.CTkButton(app, text="Open file", width=200, height=50, command=openFile)
+openFileBtn.pack(pady=(10, 10))
 
 filenameText = ct.CTkLabel(app, text=filename, font=Arial, fg_color="transparent")
-filenameText.pack()
+filenameText.pack(pady=(10, 20))
 
 app.mainloop()
